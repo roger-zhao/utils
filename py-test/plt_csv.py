e9031002b4d45ae7cfe88462a9ad5cd38995b5ec
roger zhao <general_zyj@163.com>
2016-07-18 22:46:03 +0800

    add zypkp

--- py-test/plt_csv.py
+++ py-test/plt_csv.py
@@ -24,16 +24,25 @@ for line_string in reader:
     if (ch_num < max_ch):
         pwm[ch_num].append(peroid)
         if(peroid > average[ch_num]*1.5) and (average[ch_num] != 0):
-            print "%d: ch-%d, h-%d, l-0x%x" % (line_no, ch_num, high, low)
+            print "========"
+            print line_no
+            print line_string
+            # print "%d: ch-%d, h-%d, l-0x%x" % (line_no, ch_num, high, low)
         else:
             ch_value_cnt[ch_num] += 1
             ch_value_sum[ch_num] += peroid
             average[ch_num] = ch_value_sum[ch_num]/ch_value_cnt[ch_num]
 
         if(low > 500):
-            print "%d: ch-%d, h-%d, l-0x%x" % (line_no, ch_num, high, low)
+            print "========"
+            print line_no
+            print line_string
+            # print "%d: ch-%d, h-%d, l-0x%x" % (line_no, ch_num, high, low)
         if(high > 2000) and (high < 2500):
-            print "%d: ch-%d, h-%d, l-0x%x" % (line_no, ch_num, high, low)
+            print "========"
+            print line_no
+            print line_string
+            # print "%d: ch-%d, h-%d, l-0x%x" % (line_no, ch_num, high, low)
 
 plt.title("rcin log");
 plt.xlabel("rcin points");
---
+++ systemd-smt.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+date
+PWD=`pwd`
+zypkp setting -f $(PWD)/.smt_pkp
+
+
+
